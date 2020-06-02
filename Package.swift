5bb5463d403328985105c724b647b7017fd80868
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 17:04:18 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "EC2",
-  products: [
-      .library(name: "EC2", targets: ["EC2"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "EC2", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "EC2",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "EC2", targets: ["EC2"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "EC2", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/EC2"),
+    ]
 )
